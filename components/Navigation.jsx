e72e77945d53d45e380901063343516603293287
supernovahs <harshitsinghal252@gmail.com>
2022-09-11 23:44:04 +0530

    Fix : next/link

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/router";
+import Link from "next/link";
 export default function Navigation() {
   const menu = [{ tabName: "Ads", pageName: "/" }];
 
